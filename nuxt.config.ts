bd99f56179072e763da93c55146e51bc7921bc2c
Allenyou <allenyou1126@gmail.com>
2024-03-19 20:14:11 +0800

    修复 Nuxt 配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  imports: {
-    autoImport: true
-  },
-  css: ['~/assets/css/main.css'],
-  modules: ["@nuxtjs/tailwindcss", "@element-plus/nuxt"],
-  generate: {
-    dir: 'docs'
-  },
-  publicPath: '/my-blog/_nuxt/',
-})
+	devtools: { enabled: true },
+	imports: {
+		autoImport: true,
+	},
+	css: ["~/assets/css/main.css"],
+	modules: ["@nuxtjs/tailwindcss", "@element-plus/nuxt"],
+	generate: {
+		// dir: "docs",
+	},
+	app: {
+		baseURL: "/my-blog/",
+	},
+	// publicPath: "/my-blog/_nuxt/",
+});
